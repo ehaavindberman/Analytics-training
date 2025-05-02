9732ba7dcc2e018fa490ba03366a0c02427b0e4f
Eric Haavind-Berman <ehaavindberman@gmail.com>
2025-05-02 17:07:28 +0800

    Update the title of the page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'FyreDrill',
+  description: 'Test your skills in a data fire drill',
+  // generator: 'v0.dev',
 }
 
 export default function RootLayout({
