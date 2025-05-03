6075d4a0346ccb1d7b83e4e50d2cd0c1f98c2f50
Eric Haavind-Berman <ehaavindberman@gmail.com>
2025-05-03 14:10:55 +0800

    Update AboutPage.tsx

--- app/components/AboutPage.tsx
+++ app/components/AboutPage.tsx
@@ -17,12 +17,9 @@ const AboutPage = () => {
 
       <section>
         <h2 className="text-2xl font-semibold mb-2">Why FyreDrill?</h2>
-        <p className="text-base text-gray-700 mb-4">
-          This app was born from a simple but underused idea in analytics: simulate the kinds of problems analysts actually face on the job. It's inspired by the experience of being asked, “Why is this number down?” and having to get to the root cause quickly—often under pressure.
-        </p>
         <p className="text-base text-gray-700">
-          Another important idea that FyreDrill stresses comes from the great explainer Derek Muller at Veritasium. There are 4 things it takes to become an expert 
-          (<a href="https://www.youtube.com/watch?v=5eW6Eagr9XA" target="_blank">link</a>):
+          There are 4 things it takes to become an expert according to the great explainer Derek Muller at Veritasium
+          (<a className="font-href" href="https://www.youtube.com/watch?v=5eW6Eagr9XA" target="_blank">link</a>):
         </p>
         <ul className="list-disc list-inside text-base text-gray-700 ml-4 mb-2">
           <li>Don't get too comfortable</li>
@@ -31,7 +28,9 @@ const AboutPage = () => {
           <li>Timely feedback</li>
         </ul>
         <p className="text-base text-gray-700">
-          FyreDrill gives users a repeated valid environment with timely feedback that scales in difficulty over time. Analysts are able to hone their skills whereas in real scenarios, you often never learn what's truly happened.
+          In real world scenarios, when an analyst is asked "why is this number down?" they will not have any of these. 
+          Typically, you never learn what has truly happened, and many times there are too many conflicting variables to draw useful conclusions.
+          FyreDrill gives users a safe and easy way to build their intuition through repeated trials in a valid environment with timely feedback that scales in difficulty. 
         </p>
       </section>
 
@@ -40,26 +39,35 @@ const AboutPage = () => {
         <ul className="list-disc list-inside text-base text-gray-700 ml-4">
           <li>Analytics teams looking to sharpen their investigative instincts</li>
           <li>New analysts seeking hands-on practice</li>
-          <li>Stakeholders learning when not to overreact to data blips</li>
+          <li>Anyone who would like to sharpen their critical thinking and data skills</li>
         </ul>
       </section>
 
       <section>
         <h2 className="text-2xl font-semibold mb-2">Roadmap</h2>
         <p className="text-base text-gray-700">
-          Ultimately, FyreDrill should look like a tree where any user can take multiple branching paths to get industry-specific metrics and practice in real-world scenarios. 
-          These can be created by the team here or by anyone who can upload a CSV file and some metadata. 
-          I'm also available to run bespoke trainings for tech teams, get in touch!
+          My long term vision for FyreDrill would be both a consulting company available for bespoke trainings 
+          (get in touch if you're interested!) 
+          and a library of scenarios similar to Duolingo and Khan Academy with branching paths a user can take based on the different metrics and company types they're interested in.
+          Ultimately, scenarios could be created by myself or by anyone who be user generated.
         </p>
         <ul className="list-disc list-inside text-base text-gray-700 ml-4">
-          {/* <li>Track and display wrong answer count</li> */}
           <li>Offer hints or help when users are stuck</li>
-          {/* <li>Natural language scoring of written answers</li> */}
+          <li>Improved answer AI</li>
           <li>Support for multiple datasets in a single scenario</li>
-          <li>User-generated and uploaded scenarios</li>
-          <li>Industry-specific scenarios (think, a set of scenarios for </li>
-          <li>A generalized, easy to use scenario generator tool</li>
+          <li>Industry-specific scenarios (think, a set of scenarios for B2B or two sided markets or subscription businesses, etc...)</li>
+          <li>Many more scenarios and a generalized, easy to use scenario generator tool</li>
         </ul>
+        <br></br>
+        <p className="text-base text-gray-700">
+          FyreDrill is far from perfect and I welcome any and all feedback! 
+          <br></br><br></br>
+          One of the issues I'd like to solve for is when to push back against stakeholders and not look into a question. 
+          Another issue is rating responses. 
+          Currently, answer keys do not care if you describe next steps which is hugely important for any analyst communication with stakeholders.
+          I'd also love to include a SQL mode where users can also learn or sharpen their SQL skills.
+          Finally, I think having a certificate or better way to track progress will be key. 
+        </p>
       </section>
 
       <section>
@@ -68,7 +76,7 @@ const AboutPage = () => {
           Interested in a bespoke trainin or have feedback or ideas for FyreDrill? We'd love to hear from you! Reach out directly or share your experience with your team and network to help us grow.
         </p>
         <p className="text-base text-gray-700 mt-2">
-          Email: <a href="mailto:eric@fyredrill.dev" className="text-blue-600 underline">eric@fyredrill.dev</a>
+          Email: <a href="mailto:ehb@fyredrill.dev" className="text-blue-600 underline">eric@fyredrill.dev</a>
         </p>
       </section>
     </div>
