1992a33c03a27ba694a8a5fe31f0e30276941b27
Eric Haavind-Berman <ehaavindberman@gmail.com>
2025-11-24 16:32:39 +0000

    Install PostHog

--- next.config.mjs
+++ next.config.mjs
@@ -22,6 +22,20 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://eu-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://eu.i.posthog.com/:path*',
+      },
+    ]
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 }
 
 mergeConfig(nextConfig, userConfig)
